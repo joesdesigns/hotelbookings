3e370eabb63b557d07f9c6f71e2773a91b8b7499
Joe Chappie <jfchappie@gmail.com>
2017-07-23 15:04:16 -0400

    Add list all function.
    This will give us the list of all rooms with the status attributes visible as well as their cleaning status and occupancy

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,6 +3,10 @@ class RoomsController < ApplicationController
   end
 
   def list
+    #TODO - Status shows up as a number we should create a enum or status list to use
+    #       status 0 = available, 1 = occupied, 2 = needs cleaning
+    #TODO - The storage for rooms with no storage shows up null we shold make it pretty
+    render json: { status: :ok, guests: params[:guests], bags: params[:bags], rooms: all_rooms_with_status}
   end
 
   def list_available
@@ -10,4 +14,11 @@ class RoomsController < ApplicationController
 
   def cleaning_schedule
   end
+
+  private
+
+    def all_rooms_with_status
+      @list = Room.left_outer_joins(:storages).select("rooms.*").select("storages.max_storage, storages.storage")
+    end
+
 end
